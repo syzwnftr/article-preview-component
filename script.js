b0271edd2da61a73597caba8ba9a9f50d577fb6e
Syazwan Fitri <syazfitry69@gmail.com>
2021-09-24 23:02:31 +0800

    Added function for 1200px and wider

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const componentThree = document.querySelector('.componentThree');
 const shareIcon = document.querySelector('.shareIcon');
 
+let widthLimit = 1200;
+let windowWidth = window.innerWidth;
+
 shareIcon.addEventListener('click', function() {
-    componentThree.classList.add('componentThreeActive');
+    if(windowWidth >= widthLimit ) {
+        componentThree.classList.add('componentThreeActive');
+    }  
 });
